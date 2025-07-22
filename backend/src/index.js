ff6cbb90506b59ef38b1768dd8c9fcd8af535273
bharath <bharathsai257@gmail.com>
2025-07-23 00:19:51 +0530

    5th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,8 +44,8 @@ if (
 ) {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
